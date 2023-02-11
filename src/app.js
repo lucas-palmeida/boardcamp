c397fe81160161bb2d3f19d7e1c01e1c70370422
lucas-palmeida <lucaspacheco95@gmail.com>
2023-02-11 12:01:57 -0300

    initial: desenvolvimento do arquivo principal e conexao com a db

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const server = express();
+
+server.use(cors());
+
+server.use(express.json());
+
+// server.use(routers)
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`Server running in port: ${port}`));
