66d97056b371685ac0e9d890cf9c7bc018730d51
lucas-palmeida <lucaspacheco95@gmail.com>
2023-02-12 18:02:39 -0300

    feat: implementa put na rota customers

--- src/controllers/Customers.js
+++ src/controllers/Customers.js
@@ -31,8 +31,8 @@ export async function insertCustomer(req, res) {
         
         if (customerExists.rows[0]) return res.status(409).send("O cliente informado já existe!");
 
-        const result = await db.query('INSERT INTO customers (name, phone, "cpf", birthday) VALUES ($1, $2, $3, $4)', [name, phone, cpf, birthday]);
-        console.log(result);
+        const result = await db.query('INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1, $2, $3, $4)', [name, phone, cpf, birthday]);
+        
         return res.sendStatus(201);
     } catch (error) {
         return res.status(500).send(error.message);
@@ -40,12 +40,22 @@ export async function insertCustomer(req, res) {
 }
 
 export async function updateCustomer(req, res) {
-    // const { name, phone, cpf, birthday } = req.body;
-    // const id = req.params.id;
+    const { name, phone, cpf, birthday } = req.body;
+    const id = req.params.id;
 
-    // try {
+    try {
+        const searchCustomerById = await db.query('SELECT * FROM customers WHERE id = $1', [id]);
+
+        if(!searchCustomerById.rows[0]) return res.status(404).send("Não foi possível localizar cliente com o ID informado.");
+
+        const searchCustomerByCpf = await db.query("SELECT * FROM customers WHERE id <> $1 AND cpf = $2", [id, cpf]);
         
-    // } catch (error) {
-    //     return res.status(500).send(error.message);
-    // }
+        if (searchCustomerByCpf.rows[0]) return res.status(409).send("Dados inválidos!");
+
+        const result = await db.query('UPDATE customers SET name = $1, phone = $2, cpf = $3, birthday = $4 WHERE id = $5', [name, phone, cpf, birthday, id]);
+        
+        return res.sendStatus(200);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
 }
--- src/controllers/Games.js
+++ src/controllers/Games.js
@@ -18,7 +18,7 @@ export async function insertGame(req, res) {
         if (gameExists.rows[0]) return res.status(409).send("O jogo informado já existe!");
 
         const result = await db.query('INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4)', [name, image, stockTotal, pricePerDay]);
-        console.log(result);
+        
         return res.sendStatus(201);
     } catch (error) {
         return res.status(500).send(error.message);
