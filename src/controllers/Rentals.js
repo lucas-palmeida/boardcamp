e9c84f0c800696696fffd3063694f211d55790b9
lucas-palmeida <lucaspacheco95@gmail.com>
2023-02-12 20:12:56 -0300

    fix: corrige formato de retorno do get rentals

--- src/controllers/Rentals.js
+++ src/controllers/Rentals.js
@@ -4,24 +4,15 @@ import { db } from "../database/connection.js";
 export async function listRentals(req, res) {
     try {
         const rentals = await db.query(`
-            SELECT json_build_object(
-                'id', rentals.id,
-                'customerId', rentals."customerId",
-                'gameId', rentals."gameId",
-                'rentDate', rentals."rentDate",
-                'daysRented', rentals."daysRented",
-                'returnDate', rentals."returnDate",
-                'originalPrice', rentals."originalPrice",
-                'delayFee', rentals."delayFee",
-                'customer', json_build_object(
+            SELECT rentals.*, 
+                json_build_object(
                     'id', customers.id,
                     'name', customers.name
-                ),
-                'game', json_build_object(
+                ) AS customer,
+                json_build_object(
                     'id', games.id,
                     'name', games.name
-                )
-            ) 
+                ) AS game
             FROM rentals 
             JOIN customers ON rentals."customerId" = customers.id
             JOIN games ON rentals."gameId" = games.id`
